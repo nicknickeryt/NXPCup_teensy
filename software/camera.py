23232cbf591e1629c2361476163ce2cae4728309
nicknickeryt <nick4nicker@gmail.com>
2025-03-13 19:35:20 +0100

    software: camera.py: adjust default serial port configuration

--- software/camera.py
+++ software/camera.py
@@ -4,7 +4,7 @@ from pyqtgraph.Qt import QtCore
 import serial
 
 # Serial configuration
-SERIAL_PORT = "/dev/ttyACM0"
+SERIAL_PORT = "/dev/ttyUSB0"
 BAUD_RATE = 115200  
 serial_port = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
 
